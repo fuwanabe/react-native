896c75efa9e88bbdebd167730f477970713aa6cb
fuki watanabe <fu.watanabe@live.jp>
2018-12-14 08:56:19 +0900

    Add todo list view

--- App.js
+++ App.js
@@ -6,15 +6,41 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from "react-native";
 
-type Props = {};
-export default class App extends Component<Props> {
+import TodoList from './TodoList';
+
+export default class App extends Component<{}> {
+  state = {
+    newTodo: '',
+    todos: [],
+  };
+  onChangeText(newTodo) {
+    this.setState({ newTodo });
+  }
+  onPressAdd() {
+    console.log('Press!');
+    const { newTodo } = this.state;
+    this.setState({
+      newTodo: '',
+      todos: [newTodo, ...this.state.todos]
+    });
+  }
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
-        <TextInput style={styles.form} />
+        <TextInput
+          value={this.state.newTodo}
+          style={styles.form}
+          onChangeText={text => this.onChangeText(text)}
+        />
+        <TouchableOpacity style={styles.addButton}
+        onPress={() => { this.onPressAdd()}}>
+          <Text style={styles.addButtonText}>ADD</Text>
+        </TouchableOpacity>
+        <TodoList todos={this.state.todos}/>
       </View>
     );
   }
@@ -23,10 +49,22 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    padding: 50
   },
   form: {
     padding: 30,
-    backgroundColor: '#EEE'
-  }
+    backgroundColor: "#EEE"
+  },
+  addButton: {
+    backgroundColor: "blue",
+    borderRadius: 4,
+    padding: 14,
+    marginTop: 10,
+  },
+  addButtonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+
 });
